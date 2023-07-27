89996317bfc604d6683087b29c9d33847a9a03e8
junbin-tan <junbin.tan@u.nus.edu>
2023-07-27 22:58:10 +0800

    making the auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,17 @@
 import Input from '@/components/Input'
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 
-const auth = () => {
+const Auth = () => {
+  const[email, setEmail] = useState('');
+  const[name, setName] = useState('');
+  const[password, setPassword] = useState('');
+
+  const[variant, setVariant] = useState('login');
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) => currentVariant == 'login' ? 'register' : 'login')
+  },[]);
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
         <div className='bg-black w-full h-full lg:bg-opacity-50'>
@@ -15,17 +25,39 @@ const auth = () => {
                     Sign in
                   </h2>
                   <div className='flex flex-col gap-4'>
-                    {/* <Input /> */}
+                    <Input  
+                      label='Username'
+                      onChange={(event: any) => setName(event.target.value)}
+                      id='name'
+                      type='name'
+                      value={name}
+                    />
+                    <Input  
+                      label='Email'
+                      onChange={(event: any) => setEmail(event.target.value)}
+                      id='email'
+                      type='email'
+                      value={email}
+                    />
+                    <Input  
+                      label='Password'
+                      onChange={(event: any) => setEmail(event.target.value)}
+                      id='password'
+                      type='password'
+                      value={password}
+                    />
                   </div>
-                  <div className='flex flex-col gap-4'>
-                    asdasd
-                  </div>
-                  <div className='flex flex-col gap-4'>
-                    asdasd
-                  </div>
-                  <div className='flex flex-col gap-4'>
-                    asdasd
-                  </div>
+
+                  <button className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
+                    Login
+                  </button>
+                  <p className='text-neutral-500 mt-12'>
+                    First time using Netflix?
+                    <span onClick={toggleVariant}  className='text-white ml-1 hover:underline cursor-pointer'>
+                      Create an account
+                    </span>
+                    
+                  </p>
                   
               </div>
             </div>
@@ -35,4 +67,4 @@ const auth = () => {
   )
 }
 
-export default auth
+export default Auth;
